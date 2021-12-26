b6aa3055a372bff18439220d9eee60c7832b1ea8
Victor Oliveira <eu@victoroliveira.com.br>
2021-12-26 18:38:24 -0300

    Defined new types and redefined olds

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,12 @@
-export type Image = HTMLImageElement;
-export type ImagePlacement = 'below' | 'right';
-export type ImageStack = {
-  image: Image;
+export type Coordinates = {
   x: number;
   y: number;
 };
 
-export type SelectorValue = 'automatic' | 'best-fit' | 'manual';
+export type Image = HTMLImageElement;
+export type ImagePlacement = 'below' | 'right';
+export type ImageStack = {
+  image: Image;
+} & Coordinates;
+
+export type Strategies = 'below' | 'best-fit' | 'manual' | 'right';
