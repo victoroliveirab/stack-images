520c4631596500abeb0a57a7913aedf3aa589714
Victor Oliveira <eu@victoroliveira.com.br>
2021-12-27 12:07:02 -0300

    ClipboardHandler improved to accept files from OS

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -4,32 +4,30 @@ import type { Image } from '.';
 type ClipboardHandlerCallback = (image: Image) => void;
 
 class ClipboardHandler {
-  private V_KEY = 'KeyV';
   private _callback?: ClipboardHandlerCallback;
 
   constructor() {
-    document.addEventListener('keyup', ({ code, ctrlKey }) => {
-      if (code === this.V_KEY && ctrlKey) {
-        this.handleClipboard();
-      }
-    });
+    document.onpaste = this.clipboardEventListener.bind(this);
   }
 
   set callback(callback: ClipboardHandlerCallback) {
     this._callback = callback;
   }
 
-  private async handleClipboard() {
+  private clipboardEventListener(event: ClipboardEvent) {
+    const file = this.getFileFromClipboard(event.clipboardData?.items);
+
+    if (!file) return;
     if (!this._callback)
-      throw new Error('A callback must be set prior to pasting');
+      console.warn('No ClipboardHandler callback registered');
 
-    const [item] = await navigator.clipboard.read();
-    const [type] = item.types;
+    loadImage(URL.createObjectURL(file)).then(this._callback);
+  }
 
-    const blob = await item.getType(type);
-    const img = await loadImage(URL.createObjectURL(blob));
-
-    this._callback(img);
+  private getFileFromClipboard(items?: DataTransferItemList) {
+    return Array.from(items || [])
+      .find(({ kind }) => kind === 'file')
+      ?.getAsFile();
   }
 }
 
