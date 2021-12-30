81973f60e24d7d3671c8deaeced7b6aa956a1db8
Victor Oliveira <eu@victoroliveira.com.br>
2021-12-30 18:55:31 -0300

    refactored app.ts to better handle option changes

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,84 @@
 import Canvas from './canvas';
-import pasteHandler from './paste';
-import type { SelectorValue } from '.';
+import clipboardHandler from './clipboard';
+import type { Image, Strategies } from '.';
 
 const modeSelector = <HTMLSelectElement>document.getElementById('mode');
+const rightButton = <HTMLButtonElement>document.getElementById('paste-right');
+const belowButton = <HTMLButtonElement>document.getElementById('paste-below');
 
 const canvas = new Canvas({
   limitWidth: true,
   maxWidth: window.innerWidth,
 });
-pasteHandler.callback = canvas.addImage;
+clipboardHandler.callback = canvas.addImage;
 
 modeSelector.addEventListener('change', () => {
-  const value = <SelectorValue>modeSelector.value;
+  const value = <Strategies>modeSelector.value;
 
-  if (value === 'automatic') {
-    canvas.maxWidth = 0;
+  if (value === 'below') {
+    belowOption();
     return;
   }
 
-  // TODO: work on best-fit
-  if (value === 'manual' || value === 'best-fit') {
-    canvas.limitWidth = true;
-    canvas.maxWidth = window.innerWidth;
+  if (value === 'best-fit') {
+    bestFitOption();
+    return;
+  }
+
+  if (value === 'manual') {
+    manualOption();
+    return;
+  }
+
+  if (value === 'right') {
+    rightOption();
+    return;
   }
 });
+
+// TODO: when option change, sort this.pointsOfInterest accordingly
+
+function belowOption() {
+  canvas.limitWidth = true;
+  canvas.maxWidth = window.innerWidth;
+  clipboardHandler.callback = (image: Image) => canvas.addImage(image, 'below');
+
+  hideButtons();
+}
+
+function bestFitOption() {
+  canvas.limitWidth = true;
+  canvas.maxWidth = window.innerWidth;
+  clipboardHandler.callback = canvas.addImage;
+
+  hideButtons();
+}
+
+function manualOption() {
+  canvas.limitWidth = false;
+  canvas.maxWidth = 0;
+
+  showButtons();
+}
+
+function rightOption() {
+  canvas.limitWidth = false;
+  canvas.maxWidth = 0;
+  clipboardHandler.callback = (image: Image) => canvas.addImage(image, 'right');
+
+  hideButtons();
+}
+
+function showButtons() {
+  rightButton.style.display = 'block';
+  belowButton.style.display = 'block';
+}
+
+function hideButtons() {
+  rightButton.style.display = 'none';
+  belowButton.style.display = 'none';
+}
+
+window.onload = function () {
+  belowOption();
+};
